43958db04599eee2665b6d0d2bbe80c1955a1a4c
mahesh yadav <mahesh.yadav@clouddrove.com>
2022-12-28 20:11:23 +0530

    update output.tf

--- example/point-to-site-with-certificate/output.tf
+++ example/point-to-site-with-certificate/output.tf
@@ -1,5 +1,5 @@
 output "vpn_gw_id" {
-  value       = join("", module.vpn-gateway.*.vpn_gw_id_certificate)
+  value       = join("", module.vpn.*.vpn_gw_id_certificate)
   description = "The ID of the Virtual Network Gateway."
 }
 

d6e00050c133fbfdf424e6926a5c587c5bd29d3c
Rupal Sharma <rupal.sharma@clouddrove.com>
2024-02-27 21:40:23 +0530

    fix:update the format

--- example/point-to-site-with-ad/output.tf
+++ example/point-to-site-with-ad/output.tf
@@ -1,5 +1,5 @@
 output "vpn_gw_id" {
-  value       =  module.vpn.vpn_gw_id
+  value       = module.vpn.vpn_gw_id
   description = "The ID of the Virtual Network Gateway."
 }
 
--- output.tf
+++ output.tf
@@ -1,10 +1,10 @@
 output "vpn_gw_id" {
-  value       = try( azurerm_virtual_network_gateway.vpngw[0].id, null)
+  value       = try(azurerm_virtual_network_gateway.vpngw[0].id, null)
   description = "The ID of the Virtual Network Gateway."
 }
 
 output "vpn_gw_id_certificate" {
-  value       = try( azurerm_virtual_network_gateway.vpngw2[0].id, null)
+  value       = try(azurerm_virtual_network_gateway.vpngw2[0].id, null)
   description = "The ID of the Virtual Network Gateway."
 }
 
@@ -13,6 +13,6 @@ output "local_network_gw_id" {
   description = "The ID of the Local Network Gateway."
 }
 output "local_network_gw_connection_id" {
-  value       = try( azurerm_virtual_network_gateway_connection.az-hub-onprem[*].id, null)
+  value       = try(azurerm_virtual_network_gateway_connection.az-hub-onprem[*].id, null)
   description = "The ID of the Virtual Network Gateway Connection."
 }

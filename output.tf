afcbc52c80874f068037e392f90718ccf18d3086
Vishal Sharma <vishal.sharma@clouddrove.com>
2024-09-16 15:53:54 +0530

    fix: added changes mentioned in pr comments

--- output.tf
+++ output.tf
@@ -1,18 +1,19 @@
 output "vpn_gw_id" {
-  value       = azurerm_virtual_network_gateway.vpngw[0].id
+  value       = length(azurerm_virtual_network_gateway.vpngw) > 0 ? azurerm_virtual_network_gateway.vpngw[0].id : null
   description = "The ID of the Virtual Network Gateway."
 }
 
 output "vpn_gw_id_certificate" {
-  value       = azurerm_virtual_network_gateway.vpngw2[0].id
+  value       = length(azurerm_virtual_network_gateway.vpngw2) > 0 ? azurerm_virtual_network_gateway.vpngw2[0].id : null
   description = "The ID of the Virtual Network Gateway."
 }
 
 output "local_network_gw_id" {
-  value       = azurerm_local_network_gateway.localgw[0].id
-  description = "The ID of the Local Network Gateway."
+  value       = length(azurerm_local_network_gateway.localgw) > 0 ? azurerm_local_network_gateway.localgw[0].id : null
+  description = "The ID of the Local Network Gateway"
 }
+
 output "local_network_gw_connection_id" {
-  value       = azurerm_virtual_network_gateway_connection.az-hub-onprem[0].id
-  description = "The ID of the Virtual Network Gateway Connection."
+  value       = length(azurerm_virtual_network_gateway_connection.az-hub-onprem) > 0 ? azurerm_virtual_network_gateway_connection.az-hub-onprem[0].id : null
+  description = "The ID of the Virtual Network Gateway Connection"
 }
